52d97df17e3c0656dfd8dc26841225e7b25fbf6e
Chikuu98 <chiranjonline@gmail.com>
2021-07-30 08:17:10 -0700

    Final commit Signed-off-by: Chikuu98 <chiranjonline@gmail.com>

---
+++ RAD Project/About.vb
@@ -0,0 +1,10 @@
+﻿Public Class About
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub HomeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HomeToolStripMenuItem.Click
+        Welcome.Show()
+        Me.Hide()
+    End Sub
+End Class
